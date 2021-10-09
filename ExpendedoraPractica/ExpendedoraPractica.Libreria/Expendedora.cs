9f8cf3335398d76e0c9e4b714a447f177ecdda22
micasessa <micasessa@yahoo.com>
2021-10-09 19:19:47 -0300

    creacion ejercicio Expendedora
    Creacion clases program, validador, expendedora (con sus atrib y metodos vacios)

---
+++ ExpendedoraPractica/ExpendedoraPractica.Libreria/Expendedora.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExpendedoraPractica.Libreria
+{
+    public class Expendedora
+    {
+        private List<Lata> _latas;
+        private string _proveedor;
+        private int _capacidad;
+        private double _dinero;
+        private bool _encendida;
+
+        //Des constructores
+        public Expendedora() { }
+        public Expendedora (string proveedor, int capacidad,double dinero, bool encendida)
+        {
+            _proveedor = proveedor;
+            _capacidad = capacidad;
+            _dinero = dinero;
+            _encendida = encendida;
+            _latas = new List<Lata>;
+        }
+
+        //des propiedades
+        public string Proveedor { get => _proveedor; }
+        public int Capacidad { get => _capacidad; set { _capacidad = value; } }
+        public double Dinero { get => _dinero; set { _dinero = value; } }
+        public bool Encendida { get => _encendida; }
+
+        //des metodos (vacios). chequear variables
+        public void AgregarLata(Lata)
+        { 
+        }
+
+        public Lata ExtraerLata(string cod, double precio)
+        { }
+
+        public string GetBalance()
+        { }
+
+        public int GetCapacidadRestante()
+        { 
+        }
+
+        public void EncenderMaquina()
+        { }
+
+        public bool EstaVacia()
+        { }
+    }
+}
